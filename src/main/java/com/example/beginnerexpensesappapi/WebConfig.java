eeb1fe4180b23a2694965fd90d72d9a3b31e12ec
Joel Hoefs <joel.hoefs7@gmail.com>
2023-06-16 00:24:32 +0800

    allowed cross origin requests

---
+++ src/main/java/com/example/beginnerexpensesappapi/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.beginnerexpensesappapi;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
+    }
+}
+// enables cross origin http requests
